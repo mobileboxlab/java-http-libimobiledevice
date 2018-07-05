27b1ff3d8c4a382f3f4e08962c02bf7af12e14f4
Henry Tejera <henrytejera@gmail.com>
2018-07-05 15:59:08 -0300

    Added configuration file and a valid udid

--- src/test/java/com/mobilebox/base/BaseTest.java
+++ src/test/java/com/mobilebox/base/BaseTest.java
@@ -8,7 +8,7 @@ public class BaseTest {
 
   @BeforeClass
   public void beforeClass() {
-    udid = "C1C15247-A830-46C5-A101-04F18DFBA3C6";
+    udid = "0416d649d4c1b9b57e0dae22a42344926a1dcc74";
   }
 
 }
